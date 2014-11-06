7a7be8ad50608842942ee1cfc7a44ed6617eca47
volkerha <d14123127@mydit.ie>
2014-11-06 15:07:23 +0000

    list ireland

--- list-was-queue.py
+++ list-was-queue.py
@@ -11,6 +11,11 @@ from boto.exception import SQSError
 
 conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
 
+conn2 = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
+
 rs = conn.get_all_queues()
+rs2 = conn.get_all_queues()
 for q in rs:
 	print q.id
+for q in rs2:
+  print q.id
