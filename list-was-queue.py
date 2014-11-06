7f496d1d0700a7ca4ae0161bc2a31d05a6d8f9c9
volkerha <d14123127@mydit.ie>
2014-11-06 15:33:30 +0000

    queue2

--- list-was-queue.py
+++ list-was-queue.py
@@ -9,15 +9,10 @@ from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 
-conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
+conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
 
-conn2 = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
-
+conn.create_queue("d14123127")
 rs = conn.get_all_queues()
-rs2 = conn2.get_all_queues()
-print("Region Us-East:")
+print("Region Ireland:")
 for q in rs:
 	print q.id
-print("Region Ireland:")
-for q in rs2:
-  print q.id
